37ff994bcc8ef7dd5fdc4ae76a829ae68ac12c42
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-10-12 11:30:52 -0700

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+app.get('/', (req, res) => {
+    res.render('home', { msg: 'Handlebars are Cool!' });
+})
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!')
+})
+
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
