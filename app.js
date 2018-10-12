bc4fe0fa4b38439bb5716dbbbf11d5b9074c9fe1
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-10-12 15:08:57 -0700

    creating new orgs

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express')
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/charity-center');
+
         // MODEL
 const Charity = mongoose.model('Charity', {
     organizationName: String,
-    description: String
+    description: String,
+    donationNeeded: String
 });
 
 app.listen(3000, () => {
@@ -18,16 +28,7 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// OUR MOCK ARRAY OF PROJECTS
-// let reviews = [
-//     { organizationName: "Homeless Puppies", description: "some desc" },
-//     { organizationName: "Crying Kittens", description: "some desc" }
-// ]
 
-// INDEX
-// app.get('/', (req, res) => {
-//     res.render('charity-index', { reviews: reviews });
-// })
 
 // INDEX
 app.get('/', (req, res) => {
@@ -38,4 +39,25 @@ app.get('/', (req, res) => {
         .catch(err => {
             console.log(err);
         })
+})
+
+// NEW
+app.get('/orgs/new', (req, res) => {
+    res.render('orgs-new', {});
+})
+
+// CREATE
+app.post('/orgs', (req, res) => {
+    console.log(req.body);
+     
+})
+
+// CREATE
+app.post('/orgs', (req, res) => {
+    Charity.create(req.body).then((orgs) => {
+        console.log(orgs);
+        res.redirect('/');
+    }).catch((err) => {
+        console.log(err.message);
+    })
 })
