1ea346a0ea936c8cf6f9427b35a6e4cb9d83c000
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-10-12 13:45:01 -0700

    Added MongoDB and Charity model

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/charity-center');
+        // MODEL
+const Charity = mongoose.model('Charity', {
+    organizationName: String,
+    description: String
+});
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
@@ -13,12 +19,23 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // OUR MOCK ARRAY OF PROJECTS
-let reviews = [
-    { OrganizationName: "Homeless Puppies", Description: "some desc" },
-    { OrganizationName: "Crying Kittens", Description: "some desc" }
-]
+// let reviews = [
+//     { organizationName: "Homeless Puppies", description: "some desc" },
+//     { organizationName: "Crying Kittens", description: "some desc" }
+// ]
+
+// INDEX
+// app.get('/', (req, res) => {
+//     res.render('charity-index', { reviews: reviews });
+// })
 
 // INDEX
 app.get('/', (req, res) => {
-    res.render('charity-index', { reviews: reviews });
+    Charity.find()
+        .then(orgs => {
+            res.render('charity-index', { orgs: orgs });
+        })
+        .catch(err => {
+            console.log(err);
+        })
 })
