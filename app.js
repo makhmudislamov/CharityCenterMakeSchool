eabce441575fd4c5aac71f0d8f855d41d95b4814
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-10-15 14:26:55 -0700

    updated gitignore

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.listen(port);
 //     console.log('App listening on port 3000!')
 // })
 
+// app.listen(process.env.PORT || 3000, function () {
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
