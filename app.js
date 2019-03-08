c23bf4a06a48df1942fee4dcb957b6ccbda01c18
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-03-07 23:25:44 -0800

    nope - going back to hbs

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const methodOverride = require('method-override')
 const charities = require('./controllers/charities');
 const comments = require('./controllers/comments');
 const mongoose = require('mongoose');
-const path = require('path');
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
 const bodyParser = require('body-parser');
 const app = express()
 var exphbs = require('express-handlebars');
-const pug = require('pug');
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,9 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/charity-center'
 const port = process.env.PORT || 3000;
 app.listen(port);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 charities(app);
 comments(app);
 
--- models/charity.js
+++ models/charity.js
@@ -1,41 +1,17 @@
 
-// const mongoose = require('mongoose');
-// const mongoosePaginate = require('mongoose-paginate');
-// Schema = mongoose.Schema;
-// mongoosePaginate.paginate.options = {
-//     limit: 3 // how many records on each page
-// }
-
-// // MODEL
-// const Charity = new Schema({
-//     organizationName: String,
-//     description: String,
-//     donationNeeded: String
-// })
-
-// Charity.plugin(mongoosePaginate);
-// module.exports = mongoose.model('Charity', Charity);
-
-
-"use strict";
+const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate');
-const mongoose = require('mongoose'),
-
-    Schema = mongoose.Schema;
+Schema = mongoose.Schema;
 mongoosePaginate.paginate.options = {
     limit: 3 // how many records on each page
-};
-const Charity = new Schema({
-    organizationName: { type: String, required: true }
-    , picUrl: { type: String }
-    , picUrlSq: { type: String }
-    , avatarUrl: { type: String, required: true }
-    , description: { type: String, minlength: 140, required: true }
-    , donationNeeded: { type: Number, required: true }
-}, {
-        timestamps: true
-    });
+}
 
-PetSchema.index({ name: 'text', species: 'text', favoriteFood: 'text', description: 'text' });
-PetSchema.plugin(mongoosePaginate);
+// MODEL
+const Charity = new Schema({
+    organizationName: String,
+    description: String,
+    donationNeeded: String
+})
+
+Charity.plugin(mongoosePaginate);
 module.exports = mongoose.model('Charity', Charity);
